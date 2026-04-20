563d8d34beadb5f67198a08d378476efb27e1f57
Brendan Mercer <bmercer3@student.cvtc.edu>
2026-04-19 23:51:51 -0500

    Add Database sql file

---
+++ AssembleDatabase.sql
@@ -0,0 +1,337 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: assemble
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipment`
+--
+
+DROP TABLE IF EXISTS `equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipment` (
+  `ID` smallint unsigned NOT NULL,
+  `TypeID` smallint unsigned NOT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `equipment_TypeID_idx` (`TypeID`),
+  CONSTRAINT `equipment_TypeID` FOREIGN KEY (`TypeID`) REFERENCES `equipmenttype` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment`
+--
+
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipmenttype`
+--
+
+DROP TABLE IF EXISTS `equipmenttype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipmenttype` (
+  `ID` smallint unsigned NOT NULL,
+  `Description` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipmenttype`
+--
+
+LOCK TABLES `equipmenttype` WRITE;
+/*!40000 ALTER TABLE `equipmenttype` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipmenttype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `ID` mediumint unsigned NOT NULL,
+  `TypeID` smallint unsigned NOT NULL,
+  `Count` mediumint unsigned NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `TypeID_idx` (`TypeID`),
+  CONSTRAINT `TypeID` FOREIGN KEY (`TypeID`) REFERENCES `inventorytype` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventorytype`
+--
+
+DROP TABLE IF EXISTS `inventorytype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventorytype` (
+  `ID` smallint unsigned NOT NULL,
+  `Description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventorytype`
+--
+
+LOCK TABLES `inventorytype` WRITE;
+/*!40000 ALTER TABLE `inventorytype` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventorytype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job` (
+  `ID` mediumint unsigned NOT NULL,
+  `ProductID` smallint unsigned NOT NULL,
+  `StartTime` datetime NOT NULL,
+  `ProjectedEndTime` datetime NOT NULL,
+  `ActualEndTime` datetime DEFAULT NULL,
+  `PersonnelCount` tinyint NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `job_ProductID_idx` (`ProductID`),
+  CONSTRAINT `job_ProductID` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jobequipment`
+--
+
+DROP TABLE IF EXISTS `jobequipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jobequipment` (
+  `JobID` mediumint unsigned NOT NULL,
+  `EquipmentID` smallint unsigned NOT NULL,
+  PRIMARY KEY (`JobID`,`EquipmentID`),
+  KEY `EquipmentID_idx` (`EquipmentID`),
+  CONSTRAINT `JobEquipment_EquipmentID` FOREIGN KEY (`EquipmentID`) REFERENCES `equipment` (`ID`),
+  CONSTRAINT `JobEquipment_JobID` FOREIGN KEY (`JobID`) REFERENCES `job` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jobequipment`
+--
+
+LOCK TABLES `jobequipment` WRITE;
+/*!40000 ALTER TABLE `jobequipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobequipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jobinventory`
+--
+
+DROP TABLE IF EXISTS `jobinventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jobinventory` (
+  `JobID` mediumint unsigned NOT NULL,
+  `InventoryID` mediumint unsigned NOT NULL,
+  `InventoryUsedCount` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`JobID`,`InventoryID`),
+  KEY `InventoryID_idx` (`InventoryID`),
+  CONSTRAINT `jobinventory_InventoryID` FOREIGN KEY (`InventoryID`) REFERENCES `inventory` (`ID`),
+  CONSTRAINT `jobinventory_JobID` FOREIGN KEY (`JobID`) REFERENCES `job` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jobinventory`
+--
+
+LOCK TABLES `jobinventory` WRITE;
+/*!40000 ALTER TABLE `jobinventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobinventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `ID` smallint unsigned NOT NULL,
+  `Description` varchar(50) NOT NULL,
+  `Duration` int NOT NULL,
+  `TargetPersonnelCount` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productequipment`
+--
+
+DROP TABLE IF EXISTS `productequipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productequipment` (
+  `ProductID` smallint unsigned NOT NULL,
+  `EquipmentTypeID` smallint unsigned NOT NULL,
+  `RequiredEquipmentTypeCount` tinyint unsigned DEFAULT NULL,
+  PRIMARY KEY (`ProductID`,`EquipmentTypeID`),
+  KEY `productequipment_EquipmentTypeID_idx` (`EquipmentTypeID`),
+  CONSTRAINT `productequipment_EquipmentTypeID` FOREIGN KEY (`EquipmentTypeID`) REFERENCES `equipmenttype` (`ID`),
+  CONSTRAINT `productequipment_ProductID` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productequipment`
+--
+
+LOCK TABLES `productequipment` WRITE;
+/*!40000 ALTER TABLE `productequipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productequipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productinventory`
+--
+
+DROP TABLE IF EXISTS `productinventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productinventory` (
+  `ProductID` smallint unsigned NOT NULL,
+  `InventoryTypeID` smallint unsigned NOT NULL,
+  `RequiredInventoryCount` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`ProductID`,`InventoryTypeID`),
+  KEY `InventoryTypeID_idx` (`InventoryTypeID`),
+  CONSTRAINT `productinventory_InventoryTypeID` FOREIGN KEY (`InventoryTypeID`) REFERENCES `inventorytype` (`ID`),
+  CONSTRAINT `productinventory_ProductID` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productinventory`
+--
+
+LOCK TABLES `productinventory` WRITE;
+/*!40000 ALTER TABLE `productinventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productinventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `ID` smallint unsigned NOT NULL,
+  `PermissionID` tinyint unsigned NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `PasswordHash` char(64) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `user_PermissionID_idx` (`PermissionID`),
+  CONSTRAINT `user_PermissionID` FOREIGN KEY (`PermissionID`) REFERENCES `userpermission` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userpermission`
+--
+
+DROP TABLE IF EXISTS `userpermission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userpermission` (
+  `ID` tinyint unsigned NOT NULL,
+  `Description` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userpermission`
+--
+
+LOCK TABLES `userpermission` WRITE;
+/*!40000 ALTER TABLE `userpermission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userpermission` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-19 23:46:54
