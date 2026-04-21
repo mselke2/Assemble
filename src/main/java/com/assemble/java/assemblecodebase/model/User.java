a2359ddb1216a9662dc21fc2d054ea6b36141545
Amber Selke <mselke2@student.cvtc.edu>
2026-04-21 13:16:38 -0500

    Wrote addUser() method for UserDaoImpl.java. Added apache commons codec 1.5 dependency in pom.xml. Added username property for User.java.

--- src/main/java/com/assemble/java/assemblecodebase/model/User.java
+++ src/main/java/com/assemble/java/assemblecodebase/model/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class User implements Serializable {
   private int id;
+  private String username;
   private int permissionId;
   private String firstName;
   private String lastName;
@@ -12,8 +13,9 @@ public class User implements Serializable {
   public User() {
   }
   
-  public User(int id, int permissionId, String firstName, String lastName, String passwordHash) {
+  public User(int id, String username, int permissionId, String firstName, String lastName, String passwordHash) {
     setId(id);
+    setUsername(username);
     setPermissionId(permissionId);
     setFirstName(firstName);
     setLastName(lastName);
@@ -26,6 +28,14 @@ public class User implements Serializable {
   
   public void setId(int id) {
     this.id = id;
+  }
+  
+  public String getUsername() {
+    return username;
+  }
+  
+  public void setUsername(String username) {
+    this.username = username;
   }
   
   public int getPermissionId() {
