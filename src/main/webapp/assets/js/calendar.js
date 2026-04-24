e4a8996236fe05e08d8c3107751e20def25aa5ed
Adam Baker <abaker29@student.cvtc.edu>
2026-04-24 14:16:22 -0500

    Convert calender.js to use jquery

--- src/main/webapp/assets/js/calendar.js
+++ src/main/webapp/assets/js/calendar.js
@@ -2,35 +2,44 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
-window.onload = () => {
-  // create a Date object initialized to the current date
-  let date = new Date();
+let calendarDate = new Date();
+calendarDate.setDate(1);
+
+let $monthName;
+let $dateNumbers;
+
+function populateCalendar() {
+  // create new date so we don't have to modify the original
+  let date = new Date(calendarDate);
+  // set the date to the first of the current month
+  date.setDate(1);
+
   // save the month so we can identify dates that are outside of this month
   let monthIdx = date.getMonth();
 
   // set the month header text
-  let monthName = document.querySelector("#month-name");
-  monthName.innerText = monthNames[monthIdx];
-
-  // set the date to the first of the current month
-  date.setDate(1);
+  $monthName.text(monthNames[monthIdx]);
 
   // step backwards day-by-day until we get to a Sunday (start of calendar week)
   while (date.getDay() !== 0) { // Sunday
     date.setDate(date.getDate() - 1);
   }
 
-  // for each calendar date number...
-  let dayNumbers = document.querySelectorAll(".day-number");
-  for (let dayNumber of dayNumbers) {
+  $dateNumbers.each(function() {
     // set the date number text to the date number of the stored date object
-    dayNumber.innerText = date.getDate();
+    $(this).text(date.getDate())
     // if the date object's month does not match the month we stored earlier,
     // fade it out
-    if (date.getMonth() !== monthIdx) {
-      dayNumber.classList.add("off-month");
-    }
+      .toggleClass("off-month", date.getMonth() !== monthIdx);
+
     // advance the date object forward by one day.
     date.setDate(date.getDate() + 1);
-  }
+  });
 }
+
+$(function() {
+  $monthName = $("#month-name");
+  $dateNumbers = $(".day-number");
+
+  populateCalendar();
+});
