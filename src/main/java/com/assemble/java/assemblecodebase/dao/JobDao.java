6f5036a3ddd6aa3dcfa2b4fb1664a2c3f7270f19
Adam Baker <abaker29@student.cvtc.edu>
2026-05-07 00:31:40 -0500

    Update JobDao interface

--- src/main/java/com/assemble/java/assemblecodebase/dao/JobDao.java
+++ src/main/java/com/assemble/java/assemblecodebase/dao/JobDao.java
@@ -2,10 +2,13 @@ package com.assemble.java.assemblecodebase.dao;
 
 import com.assemble.java.assemblecodebase.model.Job;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface JobDao {
-  
   int addJob(Job job);
   void updateJob(int id, Job job);
   int deleteJob(int id);
   Job retrieve(int id);
+  List<Job> retrieveForDate(LocalDate date);
 }
