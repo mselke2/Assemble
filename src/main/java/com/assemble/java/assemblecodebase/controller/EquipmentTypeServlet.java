4e715fb9729c61b1d0ac4eb96ad08dcebe73d88d
Adam Baker <abaker29@student.cvtc.edu>
2026-05-14 15:33:38 -0500

    Implement EquipmentTypeServlet doPut

--- src/main/java/com/assemble/java/assemblecodebase/controller/EquipmentTypeServlet.java
+++ src/main/java/com/assemble/java/assemblecodebase/controller/EquipmentTypeServlet.java
@@ -4,6 +4,8 @@ package com.assemble.java.assemblecodebase.controller;
 import com.assemble.java.assemblecodebase.dao.EquipmentDao;
 import com.assemble.java.assemblecodebase.dao.EquipmentDaoImpl;
 import com.assemble.java.assemblecodebase.model.EquipmentType;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -30,5 +32,30 @@ public class EquipmentTypeServlet extends HttpServlet {
 
     request.setAttribute("equipmentTypes", types);
     getServletContext().getRequestDispatcher("/equipment-type-manager.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+      int id = Integer.parseInt(request.getPathInfo().substring(1));
+
+      Gson gson = new Gson();
+      JsonObject json = gson.fromJson(request.getReader(), JsonObject.class);
+
+      String description = json.get("description").getAsString();
+      if (description.length() > 50)
+        throw new IllegalArgumentException("Invalid Description");
+
+      EquipmentType equipmentType = new EquipmentType(id, description);
+
+      EquipmentDao equipmentDao = new EquipmentDaoImpl();
+      equipmentDao.updateEquipmentType(equipmentType);
+    } catch (IllegalArgumentException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write(e.getMessage());
+    } catch (RuntimeException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      response.getWriter().write(e.getMessage());
+    }
   }
 }
