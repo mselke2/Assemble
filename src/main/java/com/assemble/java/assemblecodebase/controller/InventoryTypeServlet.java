a0b14edf496775a04575478bf1a66544a4e23ac4
Adam Baker <abaker29@student.cvtc.edu>
2026-05-14 15:33:38 -0500

    Implement InventoryTypeServlet doPut

--- src/main/java/com/assemble/java/assemblecodebase/controller/InventoryTypeServlet.java
+++ src/main/java/com/assemble/java/assemblecodebase/controller/InventoryTypeServlet.java
@@ -4,6 +4,8 @@ package com.assemble.java.assemblecodebase.controller;
 import com.assemble.java.assemblecodebase.dao.InventoryDao;
 import com.assemble.java.assemblecodebase.dao.InventoryDaoImpl;
 import com.assemble.java.assemblecodebase.model.InventoryType;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -30,5 +32,30 @@ public class InventoryTypeServlet extends HttpServlet {
 
     request.setAttribute("inventoryTypes", types);
     getServletContext().getRequestDispatcher("/inventory-type-manager.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+      int id = Integer.parseInt(request.getPathInfo().substring(1));
+
+      Gson gson = new Gson();
+      JsonObject json = gson.fromJson(request.getReader(), JsonObject.class);
+
+      String description = json.get("description").getAsString();
+      if (description.length() > 50)
+        throw new IllegalArgumentException("Invalid Description");
+      
+      InventoryType inventoryType = new InventoryType(id, description);
+
+      InventoryDao inventoryDao = new InventoryDaoImpl();
+      inventoryDao.updateInventoryType(inventoryType);
+    } catch (IllegalArgumentException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write(e.getMessage());
+    } catch (RuntimeException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      response.getWriter().write(e.getMessage());
+    }
   }
 }
